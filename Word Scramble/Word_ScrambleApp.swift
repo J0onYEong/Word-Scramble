3ebb8292ffb2cccc8483a6637e4a93e8a201e081
choijun0 <cjy00050005@gmail.com>
2022-11-09 14:18:29 +0900

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by 최준영 on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
